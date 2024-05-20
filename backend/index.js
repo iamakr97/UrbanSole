04559a156014227cb159eda8ae5bbbff5b5694fb
Amit Ranjan <122173165+iamakr97@users.noreply.github.com>
2024-05-20 10:43:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 app.use(cookieParser());
  
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.BASE_URL,
     credentials: true,
     optionsSuccessStatus: 204,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
